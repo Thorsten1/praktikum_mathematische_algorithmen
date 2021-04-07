122080030829dcf969e7f323fc89ca84b574fef6
Alexander Haase <alexander.haase@alumni.fh-aachen.de>
2021-04-07 23:15:04 +0200

    Add cross-platform argument parsing

--- src/main.py
+++ src/main.py
@@ -1,13 +1,38 @@
 #!/usr/bin/env python
 
-import sys
+import argparse
 
 from graph.graph import Graph
 
+
+def getArgs() -> argparse.Namespace:
+    """
+    Parse command line arguments.
+
+    This function parses the application's command line arguments and returns
+    the :py:class:`argparse.Namespace` to access the passed argument values. If
+    necessary, this function emits a usage message and raises an error on
+    invalid arguments or values.
+
+
+    :returns: A :py:class:`argparse.Namespace` to access the argument values.
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('graph',
+                        help='graph file to load')
+
+    # Parse the command line arguments and return the generated namespace. If
+    # an argument is unknown, or its value does not match the specification, an
+    # error message will be generated and the help message shown.
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    if len(sys.argv) == 0:
-        print("Please provide the path to the import file")
-        exit(1)
+    # Parse the command line arguments. If necessary, this function will print
+    # error messages and exits the application on errors.
+    args = getArgs()
+
     graph = Graph(0)
-    graph.import_from_file(sys.argv[1])
+    graph.import_from_file(args.graph)
     print(graph)
