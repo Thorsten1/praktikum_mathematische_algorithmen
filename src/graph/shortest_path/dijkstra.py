44d6f9a0370721cb7265210d6f2ef78ee1bd7610
Alexander Haase <alexander.haase@alumni.fh-aachen.de>
2021-05-19 18:57:01 +0200

    Fix Dijkstra performance
    Instead of searching for the vertex with minimum distance in every loop
    iteration, a sorted list will be used instead, reducing the cost from
    squared to linear performance.

--- src/graph/shortest_path/dijkstra.py
+++ src/graph/shortest_path/dijkstra.py
@@ -1,7 +1,22 @@
+import heapq
 import math
 
-from graph.graph import timeit
+from graph.graph import Vertex, timeit
 from graph.shortest_path.abstractShortestPath import ShortestPath
+
+
+class Node:
+    def __init__(self,
+                 vertex: Vertex,
+                 predecessor: Vertex,
+                 distance: float = math.inf
+                 ):
+        self.vertex = vertex
+        self.predecessor = predecessor
+        self.distance = distance
+
+    def __lt__(self, other):
+        return self.distance < other.distance
 
 
 class Dijkstra(ShortestPath):
@@ -21,34 +36,43 @@ class Dijkstra(ShortestPath):
         """
         # initialize the distances with infinity and their predecessors with
         # None because they aren't known yet
-        self.distance = {n: math.inf for n in range(self.graph.vertex_count)}
+        self.distance = [math.inf for _ in range(self.graph.vertex_count)]
         self.predecessors = [None for _ in range(self.graph.vertex_count)]
 
-        # Iterate over remaining vertexes until the remaining list is empty. The
-        # list of remaining vertexes is initialized by a full copy of distances
-        # as none has been visited before. However, the distance to the start
-        # vertex is zero, as ... its the beginning of course.
-        self.distance[start_vertex] = 0
-        remaining = self.distance.copy()
+        # Initialize the list of remaining vertexes to visit, which will be used
+        # as heapqueue, a sorted list, having the node with the lowest priority
+        # always first. However, the distance to the start vertex is zero, as
+        # its the beginning of course.
+        remaining = [Node(self.graph.vertexes.get(n), None, math.inf)
+                     for n in range(self.graph.vertex_count)]
+        heapq.heappush(remaining,
+                       Node(self.graph.vertexes.get(start_vertex), None, 0))
+
+        # Iterate over remaining vertexes until the remaining list is empty.
         while remaining:
             # From the list of remaining vertexes, get the one with the minimum
-            # known distance. It will be removed from the remaining list and its
-            # final distance copied into the  return dictionary.
-            cur = self.graph.vertexes.get(min(remaining, key=remaining.get))
-            self.distance[cur] = remaining[cur]
-            del remaining[cur]
+            # known distance. It will be removed from the remaining list.
+            cur = heapq.heappop(remaining)
+            if self.distance[cur.vertex.value] != math.inf:
+                continue
+
+            # Persist the distance and predecessor of this vertex in the global
+            # variabled returned at the end of the method, as its values won't
+            # change anymore.
+            self.distance[cur.vertex.value] = cur.distance
+            self.predecessors[cur.vertex.value] = cur.predecessor
 
             # Iterate over all connected vertexes not known yet and update the
             # related distances, if these are better than the existing ones.
-            for e in cur.edges:
-                if e.end in remaining:
+            for e in cur.vertex.edges:
+                if self.distance[e.end.value] == math.inf:
                     if e.weight < 0:
                         print('Negative Kante im Graphen. Das Ergebnis ist ' +
                               ' ggf. nicht optimal!')
 
-                    tmp = self.distance[cur] + e.weight
-                    if tmp < remaining[e.end]:
-                        remaining[e.end] = tmp
-                        self.predecessors[e.end.value] = cur.value
+                    heapq.heappush(remaining,
+                                   Node(e.end,
+                                        cur.vertex,
+                                        cur.distance + e.weight))
 
-        return self.distance.values(), self.predecessors
+        return self.distance, self.predecessors
