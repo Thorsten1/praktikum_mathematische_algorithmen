fee4d1e730477be00fcad7d7c7d9e79d3f77ebc2
Alexander Haase <alexander.haase@alumni.fh-aachen.de>
2021-06-22 22:43:50 +0200

    Fix b-Flow generation

--- src/graph/flow/cost_minimal/cycle_canceling.py
+++ src/graph/flow/cost_minimal/cycle_canceling.py
@@ -27,10 +27,10 @@ class CycleCanceling(AbstractCostminFlow):
         """
         # Generate a super source and target to accumulate multiple ones into
         # just a single one.
-        s = BalanceVertex(flow.vertex_count)
+        s = flow.add_vertex(flow.vertex_count)
         for sv in filter(lambda v: v.balance > 0, list(flow.vertexes.values())):
             flow.add_existing_edge(FlowEdge(s, sv, sv.balance))
-        t = BalanceVertex(flow.vertex_count)
+        t = flow.add_vertex(flow.vertex_count)
         for tv in filter(lambda v: v.balance < 0, list(flow.vertexes.values())):
             flow.add_existing_edge(FlowEdge(tv, t, tv.balance * -1))
 
@@ -38,7 +38,9 @@ class CycleCanceling(AbstractCostminFlow):
         # edges to / from source and target use their full capacity, its a
         # b-Flow and it can be returned, except for the super vertexes.
         maxFlow = EdmondsKarp(flow)(s, t)
-        if all(map(lambda e: e.capacity == e.flow, s.edges | t.edges)):
+        if all(map(lambda e: e.capacity == e.flow,
+                   filter(lambda e: e.start == s or e.end == t,
+                          flow._flatten_edges()))):
             maxFlow.remove_vertex(maxFlow.vertex_count - 1)
             maxFlow.remove_vertex(maxFlow.vertex_count - 1)
             return maxFlow
@@ -53,3 +55,5 @@ class CycleCanceling(AbstractCostminFlow):
         """
         # Step 1: Generate initial flow
         flow = self.__bFlow(self.graph)
+        if not flow:
+            return
